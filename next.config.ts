d7e8f5df6e4755e3ad8ee000a224f06140f0f972
Dev0sman <signorabdi@gmail.com>
2025-07-19 13:40:28 +0300

    Revert "second commit"
    This reverts commit 6cff85c7b3ed9d90521850de07e23b4bc2ebb045.

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        port: "",
-        pathname: "/**",
-      },
-    ],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
+
+// added by create cloudflare to enable calling `getCloudflareContext()` in `next dev`
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+initOpenNextCloudflareForDev();
